956d12db70059ad0b6b08ab908986bdfd3259eeb
pablo.callejo <aa.pablo.callejo@institutosanjuandelacruz.com>
2021-02-22 13:20:40 +0100

    validacion

--- prueba1/src/cambio1/cambio1.java
+++ prueba1/src/cambio1/cambio1.java
@@ -3,8 +3,9 @@ package cambio1;
 public class cambio1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Pruebas Git. Clase Cambio1");
 
 	}
 
 }
+
--- prueba1/src/prueba1/Principal.java
+++ prueba1/src/prueba1/Principal.java
@@ -4,6 +4,11 @@ public class Principal {
 
 	public static void main(String[] args) {
 		System.out.println("Hola mundo java-git");
+		System.out.println("Hola mundo java-git");
+		System.out.println("Hola mundo java-git");
+		System.out.println("Pruebas Git. Clase Cambio1");
+
+	
 
 	}
 
